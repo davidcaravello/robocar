48a6e7b0c9c5ddf69555df55ee33d65003387dd0
David Caravello <dcaravello@gmail.com>
2018-04-27 04:06:33 -0500

    consolidation is now fully sequential

--- tubcon.py
+++ tubcon.py
@@ -1,112 +1,102 @@
 import json
 import os
 import shutil
-import re
 import sys
 
 outdir = 'out/'
-PAD_FORMAT = '{:0>8}'
 JSON_KEY = 'cam/image_array'
 
-# NUMS_REGEX = re.compile('([0-9]+)')
+def get_tubs_to_process(indir):
+    tubs = {}
+    # Process all dirs that start with Tub
+    for root, dirs, files in os.walk(indir):
+        if not os.path.basename(root).startswith('tub'):
+            print("Skipping files in [{}] (doesn't start with 'tub')".format(root))
+            continue
 
-tubcount = 1000
+        print('Discovering files in [{}]'.format(root))
+        records = []
+        for file in files:
+            if file == 'meta.json':
+                # Assumption, is a meta.json per tub, and they are all exactly the same
+                continue
 
-def pad_num_beg(filename):
-    num = ''
-    for char in filename:
-        if char.isdigit():
-            num += char
-        else:
-            break
+            elif file.endswith(".jpg"):
+                # Will get jpg file details from json records
+                continue
 
-    return PAD_FORMAT.format(num) + filename[len(num):]
+            elif file.endswith('.json'):
+                with open(root + os.path.sep + file, 'rt') as f:
+                    json_contents = json.loads(f.read())
 
-def pad_num_mid(filename):
-    num = ''
-    started = False
-    for char in filename:
-        if char.isdigit():
-            num += char
-            started = True
-        elif started:
-            break
+                records.append((file, json_contents))
+        
+        tubs[root] = records
 
-    return filename.replace(num, PAD_FORMAT.format(num))
+    return tubs
+        
+def consolidate(tubs, outdir):
+    record_count = 0
+    for tub_dir in tubs.keys():
+        records = tubs[tub_dir]
+        # This lambda will extract the digits from the record_####.json filename, and then sort the filenames
+        # based on the int representation of the digits (as opposed to alphabetically)
+        records.sort(key=lambda x: int(x[0][:-5].split('_')[1]))
+        record_count += len(records)
 
-def contruct_filename(root, filename, pad_func):
-    newfilename = pad_func(filename)
-    newfile = outdir + os.path.sep + os.path.basename(root) + '-' + newfilename
-    newfile = os.path.abspath(newfile)
+    print('Found [{}] records in the tubs'.format(record_count))
 
-    return newfile
+    max_digits = len(str(record_count)) + 1
+    record_filename_template = 'record_{:0>' + str(max_digits) + '}.json'
+    image_filename_template = '{:0>' + str(max_digits) + '}_cam-image_array_.jpg'
+    progress_template = '\r[{:>7.2f}%][{:>' + str(max_digits-1) + '}/{}] Processing [{}]       '
 
-def handle_meta(root, file):
-    curfile = os.path.abspath(root + os.path.sep + file)
-    shutil.copy(curfile, outdir + os.path.sep + file)
+    record_index = 0
+    for tub_dir in tubs.keys():
+        # Lets create our meta file if it doesn't exist
+        dst_metafile = os.path.join(outdir, 'meta.json')
+        if not os.path.exists(dst_metafile):
+            print('Creating meta.json')
+            src_metafile = os.path.join(tub_dir, 'meta.json')
+            shutil.copy(src_metafile, dst_metafile)
 
-def handle_image(root, file):
-    curfile = os.path.abspath(root + os.path.sep + file)
-    newfile = contruct_filename(root, file, pad_num_beg)
-    shutil.copy(curfile, newfile)
+        records = tubs[tub_dir]
+        # print('\nProcessing [{}] records in [{}] - beginning index [{}]'.format(len(records), tub_dir, record_index+1))
 
-def handle_json(root, file):
-    curfile = os.path.abspath(root + os.path.sep + file)
+        for i, record in enumerate(records):
+            record_index += 1
 
-    with open(curfile, 'rt') as f:
-        contents = f.read()
+            record_filename = record[0]
+            record_json = record[1]
+
+            image_filename = record[1][JSON_KEY]
+
+            new_record_filename = record_filename_template.format(record_index)
+            new_image_filename = image_filename_template.format(record_index)
+
+            record_json[JSON_KEY] = new_image_filename
+            with open(outdir + os.path.sep + new_record_filename, 'wt') as f:
+                f.write(json.dumps(record_json))
+
+            src_img_filename = os.path.join(tub_dir, image_filename)
+            dst_img_file = os.path.join(outdir, new_image_filename)
+            shutil.copy(src_img_filename, dst_img_file)
+            # print('[{}] -> [{}] and [{}] -> [{}]'.format(record_filename, new_record_filename, image_filename, new_image_filename))
+            print(progress_template.format((record_index / record_count)*100, record_index, record_count, os.path.join(tub_dir, record_filename)), end='')
     
-    json_parsed = json.loads(contents)
-    new_json_val = os.path.basename(root) + '-' + pad_num_beg(json_parsed[JSON_KEY])
-
-    if new_json_val.startswith('.\\'):
-        new_json_val = new_json_val[2:]
-
-    json_parsed[JSON_KEY] = new_json_val
-
-    #newfile = contruct_filename(root, file, pad_num_mid)
-    global tubcount
-    newfilename = str(tubcount) + pad_num_mid(file)
-    newfilename = newfilename[:-5] # hack remove json
-    newfile = outdir + os.path.sep + newfilename + '.json'
-    newfile = os.path.abspath(newfile)
-
-    with open(newfile, 'wt') as f:
-        f.write(json.dumps(json_parsed))
-
-# record_$$$_file.json
+    print('\nFinished creating records in [{}]'.format(os.path.abspath(outdir)))
 
 def main():
     # Ensure the output directory exists
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
-    # Process all dirs that start with Tub
     indir = '.'
     if len(sys.argv) > 1:
         indir = sys.argv[1]
 
-    for root, dirs, files in os.walk(indir):
-        if not os.path.basename(root).startswith('tub'):
-            print('Skipping [{}]'.format(root))
-            continue
-
-        print('\nProcessing Files in [{}]'.format(root))
-        global tubcount
-        tubcount += 1000
-        for file in files:
-            if file == 'meta.json':
-                handle_meta(root, file)
-
-            elif file.endswith(".jpg"):
-                handle_image(root, file)
-
-            elif file.endswith('.json'):
-                handle_json(root, file)
-
-            # printing and flushing may slow down processing
-            print('.', end='')
-            sys.stdout.flush()
+    tubs = get_tubs_to_process(indir)
+    consolidate(tubs, outdir)
 
 if __name__ == '__main__':
     main()
